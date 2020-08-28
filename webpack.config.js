fe67db21440da7aef6cd912c8e0eb0bc3398f2a9
narendrasinghrathore <rathoreofficialemail@gmail.com>
2020-08-28 16:04:02 +0530

    fix: fs error when building to package

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   target: 'web',
   mode: 'production',
   entry: './dist/index',
+  node: { fs: 'empty' },
   output: {
     filename: 'sketch.js',
     path: path.resolve(process.cwd(), 'dist'),
