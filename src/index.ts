6e4c4a00d77c5866374783014b2941bb8482c8c9
narendrasinghrathore <rathoreofficialemail@gmail.com>
2020-09-25 09:16:48 +0530

    1) https://github.com/narendrasinghrathore/sketch-assistant-spell-check/issues/1#issuecomment-694072562 2) https://github.com/narendrasinghrathore/sketch-assistant-spell-check/issues/1#issuecomment-694173367
     Performance improvement by 1 sec.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,14 @@ import {
   RuleDefinition,
 } from '@sketch-hq/sketch-assistant-types';
 import data from './data/data.json';
+const p = require('../package.json');
+/**
+ * ### Data source of words [en-US]
+ *  List of all the words  against which we compare the given 
+ * word from sketch file and check if it is spelled correctly.
+ */
 const list = data.list;
+const listLowerCase = data.list.map(item => item.toLowerCase());
 
 const specialCharacters = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', "^", "_", '`', '{', '|', '}', '~'];
 
@@ -18,33 +25,45 @@ const suggestionList = (word: string, maxWords: number = 5): string[] => {
 
   if (!word) return [];
 
-  list.slice(list.indexOf(word.charAt(0))).forEach((item: string, index: number, array) => {
+  // Get start index with startsWith and get only maxWords items from our array of words for suggestion.
+  const indexStartSliceFrom = list.findIndex(item => item.startsWith(word));
 
-    const itemFound = item.toLowerCase().startsWith(word.toLowerCase());
+  if (indexStartSliceFrom === -1) return [];
 
-    if (itemFound) {
-      similarWords.push(array[index]);
-    }
+  const indexStartSliceTo = indexStartSliceFrom + maxWords;
 
-  });
+  const wordsSuggesstionList = list.slice(indexStartSliceFrom, indexStartSliceTo);
 
-  return similarWords.slice(0, maxWords);
-}
+  if (wordsSuggesstionList.length === 0) return [];
 
-const textNoLoremIpsum: RuleDefinition = {
+  for (let i = 0; i < maxWords; i++) {
+    similarWords.push(wordsSuggesstionList[i]);
+  }
+
+  return similarWords;
+
+};
+
+const spellChecker: RuleDefinition = {
   rule: async (context) => {
 
-    const { utils } = context
+    const { utils } = context;
+
+    // source data from files
+    const text = utils.objects.text;
 
     // Iterate
-    for (const layer of utils.objects.text) {
+    for (const layer of text) {
 
       const words = layer.attributedString.string;
       const values = words
-        .replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, " ").replace(/\n/gi, " ").trim().split(" ");
+        .replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, " ")
+        .replace(/\n/gi, " ")
+        .trim()
+        .split(" ");
+      const valuesLowerCase = values.map(item => item && item.toLowerCase());
 
-      values.forEach((word) => {
-
+      valuesLowerCase.forEach((word, index) => {
         /**
          * For example: 
          * #Text/#Sample$ will be
@@ -56,49 +75,42 @@ const textNoLoremIpsum: RuleDefinition = {
         // Check if we not testing a blank value, if yes return from here.
         if (!word) return;
 
-
-
-        // Lowercase the word and check if the string end with "." i.e. and remove if yes
-        const transformedWord = word.toLowerCase();
-
         // Check if not a number
         if (!isNaN(word as any)) return;
 
         // Check if not a special character
-        if (specialCharacters.indexOf(transformedWord) !== -1) return;
+        if (specialCharacters.indexOf(word) !== -1) return;
 
         (async () => {
-          const misspelled = list.find(w => w.toLowerCase() === transformedWord) !== undefined ? false : true;
+          const misspelled = listLowerCase.find(w => w === word) !== undefined ? false : true;
 
           const suggestions: string[] = [];
 
           // Report
           if (misspelled) {
-            suggestions.push(...suggestionList(transformedWord, 10));
+            suggestions.push(...suggestionList(word, 10));
             // Return suggestion(s) for word 
-            utils.report(`${word}${suggestions.length > 0 ? '. Did you mean : ' + suggestions.join(", ") : ''}`, layer)
+            utils.report(`${values[index]}${suggestions.length > 0 ? '. Did you mean : ' + suggestions.join(", ") : ''}`, layer)
           }
         })();
-
-
 
       });
 
     }
   },
-  name: 'sketch-assistant/spell-check',
-  title: 'Spelling Error(s)',
+  name: p.name,
+  title: 'Use correct spellings.',
   description:
-    'Reports a violation when text layers contain incorrect spelling(s)',
+    'Use correct spelling in your text layer(s)',
 }
 
 const assistant: AssistantPackage = async () => {
   return {
-    name: 'sketch-assistant',
-    rules: [textNoLoremIpsum],
+    name: p.name,
+    rules: [spellChecker],
     config: {
       rules: {
-        'sketch-assistant/spell-check': {
+        [p.name]: {
           active: true,
         },
       },
